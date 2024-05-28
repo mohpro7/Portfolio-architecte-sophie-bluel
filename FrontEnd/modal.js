80dd58422b7bf22e6a020dcfe038be97594fceaa
Mezouar Mohamed <mohmezpro@etik.com>
2024-05-29 00:57:29 +0200

    comportement de afficher et fermer la modal fini

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -1,10 +1,30 @@
-const fileInput = document.getElementById('file-input');
-const fileButton = document.querySelector('.custom-input-file')
+document.addEventListener('DOMContentLoaded', () =>{
+    const fileInput = document.getElementById('file-input');
+    const fileButton = document.querySelector('.custom-input-file');
+    const buttonModal = document.getElementById('modal-btn');
+    const modalContainer = document.getElementById('modal-container');
+    const closeModal = document.querySelector('.close-modal');
 
-fileButton.addEventListener('click', () => {
-    fileInput.click();
+
+
+    buttonModal.addEventListener('click', () => {
+        modalContainer.style.display = 'flex';
+    });
+
+    closeModal.addEventListener('click', () => {
+        modalContainer.style.display = 'none';
+    });
+
+    fileButton.addEventListener('click', () => {
+        fileInput.click();
+    });
+
+    window.addEventListener('click', (event) => {
+        if (event.target === modalContainer) {
+            modalContainer.style.display = 'none';
+        }
+    });
+
 });
-
-
 
 
